35333693c46bc06f04a6f3f3fa0e8cab37dac83f
R.1984 <1984bot+git@protonmail.com>
2024-04-12 03:22:35 -0500

    add chart

---
+++ docs/buttons.js
@@ -0,0 +1,11 @@
+function toggleChart() {
+    const chartEmbed = document.getElementById("chartEmbed")
+    console.log("📈");
+    chartEmbed.classList.toggle("fade-in")
+}
+
+function toggleGameCart01() {
+    const gamecart01 = document.getElementById("gamecart01")
+    console.log("🎮");
+    gamecart01.classList.toggle("fade-in")
+}
